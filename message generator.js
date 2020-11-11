631965f18376ee489d2c9d29ea97f535b13ce741
rotemWeinberg <rotemwei@umich.edu>
2020-11-11 18:06:22 -0500

    functions added

--- message generator.js
+++ message generator.js
@@ -1,5 +1,17 @@
-const nounArr = [];
-const verbArr = [];
-const adjArr = [];
+const nounArr = ['book', 'table', 'chair', 'computer', 'piano'];
+const verbArr = ['go', 'paint', 'play', 'wear'];
+const adjArr = ['pretty', 'sleepy', 'fast', 'cool', 'sweet', 'green'];
+
+function randomIndex(arr) {
+    return Math.floor(Math.random()*arr.length)
+}
+
+function generateMessage () {
+    const noun = nounArr[randomIndex(nounArr)];
+    const verb = verbArr[randomIndex(verbArr)];
+    const adj = adjArr[randomIndex(adjArr)];
+    return `I want to ${verb} but my ${adj} ${noun} is not around`;
+}
 
 console.log('This is my message generator project');
+console.log(generateMessage());
